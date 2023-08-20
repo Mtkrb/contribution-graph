23b6ffab7698d0d8144c9c9a5b40fee902a879d5
Bektemir Abilov <younghearth@gmail.com>
2023-08-20 17:15:23 +0600

    removed some code added fetch for contributions

--- script.js
+++ script.js
@@ -1,51 +1,22 @@
 // script.js
+let url = `https://dpg.gg/test/calendar.json`
 const calendarContainer = document.getElementById("calendarContainer")
 const startDate = new Date()
+console.log(startDate)
 startDate.setDate(startDate.getDate() - 357) // Передвигаемся назад на 357 дней
+console.log(startDate)
 
-async function fetchContributions() {
-  try {
-    const response = await fetch("https://dpg.gg/test/calendar.json")
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.error("Error fetching contributions:", error)
-    return {}
+// get data
+const getData = async function (url) {
+  const result = await fetch(url)
+  if (!result.ok) {
+    throw new Error(`Ошибка фетч ${url} статус: ${result.status}`)
   }
+  return await result.json()
 }
 
-class CalendarDay {
-  constructor() {
-    this.element = document.createElement("div")
-    this.element.classList.add("calendar-day")
-    this.generateSquares()
-  }
+getData(url).then((data) => {
+  console.log(data)
+})
 
-  generateSquares() {
-    let currentDate = new Date(startDate)
-
-    for (let week = 0; week < 51; week++) {
-      const square = document.createElement("div")
-      square.classList.add("calendar-square")
-      const dateString = currentDate.toISOString().split("T")[0]
-      const activityLevel = contributions[dateString] || 0
-      square.classList.add(`level-${activityLevel}`)
-      if (currentDate.toDateString() === new Date().toDateString()) {
-        square.classList.add("current-day")
-      }
-      this.element.appendChild(square)
-      currentDate.setDate(currentDate.getDate() + 1)
-    }
-  }
-}
-
-async function init() {
-  const contributions = await fetchContributions()
-
-  for (let day = 0; day < 7; day++) {
-    const calendarDay = new CalendarDay()
-    calendarContainer.appendChild(calendarDay.element)
-  }
-}
-
-init()
+// calendar render
